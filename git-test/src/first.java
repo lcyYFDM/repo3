849e4d571f3cda682f2d6227988e7f58dfda57fa
lcy <2579289381@qq.com>
2020-08-05 07:59:08 +0800

    提交git-test工程

---
+++ git-test/src/first.java
@@ -0,0 +1,16 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello project ");
+        System.out.println("hello project ");
+        System.out.println("hello project ");
+        System.out.println("hello project ");
+        System.out.println("hello project ");
+        System.out.println("hello project ");
+        System.out.println("hello project ");
+    }
+
+    public void test() {
+        int i = 0;
+        i += 6;
+    }
+}
